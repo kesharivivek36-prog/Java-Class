7d48acda08fc906b1c7b4cb0cb8464b107e79509
vivek keshari <kesharivivek36@gmail.com>
2026-08-13 14:04:48 +0530

    learning JavaScript

---
+++ script.js
@@ -0,0 +1,419 @@
+/*for(let i=1; i<=10; i++){
+    console.log(i);
+}*/
+
+/*for(let i=1; i<=10; i++){
+    if(i%2 ===0){
+ console.log(i);
+    }
+   }*/
+
+    /*for (let i= 1; i<=20; i++){
+        if(i%2 !==0){
+            console.log(i);
+        }
+    }*/
+
+        /*let number =7;
+        for(let i =1; i<=10; i++){
+            console.log(number * i);
+        }*/
+
+            /*let i= 1;
+            while (i<=100){
+                console.log(i);
+                i++;
+            }*/
+
+                /*let i=1;
+                while (i<=20){
+                    if (i %2 ===0){
+                        console.log(i);
+                    }
+                    i++;
+                }*/
+
+                    /*let i=5;
+                    while (i>=1){
+                        console.log(i);
+                        i--;
+                    }*/
+
+                        /*let number =5;
+                        let i=1;
+                        while(i<=10){
+                            console.log(number + "x" + i+ "=" + (number *i) );
+                            i++;
+                        }*/
+                       //-------------========================================
+
+                       /*let number =10;
+                       let i=1;
+                       while ( i<=10){
+                        console.log(number + "x"+ i+ "=" + (number* i) )
+                        i++;
+                       }*/
+//========================================================
+
+
+/*let i=10;
+while (i<=50){
+    console.log(i);
+    i++;
+}*/
+
+//=============================================
+
+/*let i= 20;
+while(i >= 1){
+    console.log(i);
+    i--;
+}*/
+//===============================================================
+
+
+
+                 /*let i=1;
+                while (i<=20){
+                    if (i %2 ===0){
+                        console.log(i);
+                    }
+                    i++;
+                }*/
+
+                    /*let i= 1; 
+                    while (i<=50){
+                    if(i % 2 !== 0){
+                     console.log(i);
+                    }
+                    i++
+                    }*/
+
+//============================================================                    
+
+/*let number =7;
+let i=1;
+while(i<=10){
+    console.log ( number + "x" + i + "="  + (number*i) );
+    i++
+}*/
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i += 2;
+// }
+//=====================================
+//do while 
+
+/*let i = 1;
+do {
+    console.log(i);
+    i++;
+
+} while(i<=5);*/
+//======================================
+/*let i = 5;
+do {
+    console.log(i);
+    i--;
+} while(i>=1);*/
+
+//=========================================
+
+/*let i = 1;
+do {
+    console.log (i);
+    i += 2 ;
+
+} while(i<=10);*/
+
+//=========================================
+/*let number = 5;
+let i = 1;
+do {
+    console.log(number + "x"+ i + "=" (number * i));
+    i++;
+} while(i <= 10); */
+//=============================================
+
+/*let number = 20;
+let i = 1;
+do {
+    console.log(number * i);
+    i++;
+} while (i<=10);*/
+ //===========================================
+
+ /*let play = true;
+
+do {
+    console.log("Game Started");
+} while (play);*/
+ //============================================
+
+ /*let i = 1;
+ do {
+    console.log (i);
+    i++;
+
+ } while (i<=10);*/
+
+//=============================================
+
+/*let i = 10;
+do {
+    console.log(i);
+    i--;
+} while(i>=1);*/
+
+//=============================================
+
+/*let i = 20;
+do {
+    console.log(i);
+    i--;
+} while(i>=1);*/
+ //==========================================
+
+/* let i =2;
+ do {
+    console.log(i);
+    i+=2;
+ }while(i<=20);*/
+
+ //=========================================
+ /*let i = 2;
+ do {
+    console.log (i+1);
+    i +=1;
+ }while (i<=19);*/
+ //============================================
+
+ // ============break=======================
+
+/*let i = 1;
+
+while (i <= 10) {
+
+    console.log(i);
+
+    if (i === 6) {
+        break;
+    }
+
+    i++;
+}*/
+//==============================================
+/*for (let i = 1; i<=20; i++){
+    if (i===10){
+        break;
+    }
+    console.log(i);
+}*/
+//==================continue======================
+
+/*let marks = [80, 0, 75, 78, 0, 60];
+for (let i = 0; i< marks.length; i++){
+    if (marks[i] ===0){
+        continue;
+    }
+    console.log(marks[i]);
+}*/
+//=================================================
+/*for (let i=1; i<=20; i++){
+    if (i===10){
+        continue;
+    }
+    console.log(i);
+}*/
+//================================================
+/*for (let i =1; 1<=30; i++){
+    if (i=== 15){
+        continue;
+    }
+    console.log(i);
+}*/
+//====================================================
+//================Nested Loops=====================
+/*for (let i=1; i<=3; i++){
+
+    for (let j=1; j<=5; j++){
+        console.log("*");
+    }
+}*/
+//==================================================
+
+
+
+// for (let i = 1; i <= 3; i++) {
+
+//     let row = "";
+
+//     for (let j = 1; j <= 5; j++) {
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+//=====================================================
+
+// for (let i = 1; i <= 3; i++) {
+
+//     let row = "";
+
+//     for (let j = 1; j <= 5; j++) {
+//         row += "*";
+//     }
+
+//     console.log(row);
+// }
+//=====================================================
+
+// for (let i= 1; i<=2; i++){
+//     for (let j=1; j<=3; j++){
+//         console.log(i,j);
+//     }
+// }
+//===============================================            
+
+// for (let i=1; i<=4; i++){
+//     for(let j=1; j<=5; j++){
+//         process.stdout.write("*");
+//     }
+//     console.log();
+// }
+//================================================
+
+// for (let i=1; i<=3; i++){
+
+//     for(let j=1; j<=3; j++){
+//         process.stdout.write(j + " ");
+//     }
+//     console.log();
+// }
+
+//=================================================
+
+// for(let i=1; i<=5;i++){
+    
+//     for(let j=1; j<=4; j++){
+//         process.stdout.write(j + " ");
+//     }
+//     console.log();
+// }
+//================================================
+//  for (let i=1; i<=5; i++){
+
+//     for(let j=1; j<=i;j++){
+//         process.stdout.write("*");
+//     }
+//     console.log();
+//  }
+
+//===============================================
+
+
+
+// for (let i = 5; i >= 1; i--) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write("*");
+//     }
+
+//     console.log();
+// }
+//=================================================
+
+// for (let i=6; i>=1; i-- ){
+
+//     for (let j=1; j<=i; j++){
+//         process.stdout.write("*");
+
+//     }
+//     console.log();
+// }
+//==================================================
+
+// for (let i= 1; i<=6; i++){
+//     let stars;
+//     if (i % 2 === 1){
+//         stars = 6;
+//     } else {
+//         stars = 5;
+//     }
+//     for (let j =1; j <= stars; j++){
+//         process .stdout.write("*");
+//     }
+//     console.log();
+// }
+//================Array=====================
+// let colors = ["Red", "Blue", "Green", "Yellow"];
+// console.log(colors[1]);
+
+//================================================
+
+// let numbers =[10, 20, 30, 40, 50];
+// console.log(numbers[3]);
+
+//===============================================
+// let cities =["lucknow", "delhi", "mumbai", "patna", "jaipur"];
+// console.log(cities.length);
+
+//================================================
+// let fruits = ["apple","mango", "banana"]
+// fruits[1] ="orange";
+// console.log(fruits);
+//==============================================
+// let names = ["Vivek", "Shivam", "Abhi"]
+// names[0] = "Abhi2";
+// console.log(names);
+//==================swap===================
+
+// let fruits =[ "Mango", "banana", "apple"];
+// let temp = fruits [0];
+// fruits[0] = fruits[2];
+// fruits[2] =temp;
+
+// console.log(fruits);
+
+//================================================
+
+// let Names =["Ujjwal","Munna", "Pooja"]
+// let temp = Names [1];
+// Names [1] = Names[2];
+// Names [2] = temp;
+
+// console.log(Names);
+//===================push============================
+
+// let fruits = ["Apple", "Mango"];
+//  fruits.push("Banana");
+//  console.log(fruits);
+//=====================pop===========================
+
+// let fruits =["apple", "mango", "banana"];
+// fruits.pop();
+
+// console.log(fruits);
+//================unshift===========================
+// let fruits = ["Mango", "Banana"];
+// fruits.unshift("apple");
+// console.log(fruits);
+
+//============================================
+// let values =["10","20", "30"];
+// values.unshift("5");
+// console.log(values);
+//================shift=======================
+// let numbers =["30", "40","50"]
+// numbers.shift();
+// console.log(numbers)
+
+
+
+
+
+
+
+
+
